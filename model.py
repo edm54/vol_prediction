55537991514669f4a89e1b4a00c09f4fb378d409
Eric Miller <edm54@case.edu>
2020-11-25 12:05:18 -0500

    Added combined model which uses the initial volatility to help predict the change

--- model.py
+++ model.py
@@ -1,41 +1,64 @@
 import tensorflow as tf
 from data import Data
 import numpy as np
-import tensorflow_transform as tft
-# Supress deprecation warnings
 import logging
 logging.getLogger('tensorflow').disabled = True
-# tf.compat.v1.disable_eager_execution()
 from statistics import mean, variance, sqrt
 import matplotlib.pyplot as plt
 
 class Model:
 
-    def __init__(self, data: Data):
+    def __init__(self, data: Data, combined=False, train=True):
         self.data = data
+        self.vix_var, self.vix_mean = self.get_vix_params()
 
-        # Need to preprocess the data
-        # Several options:
-        # 1) Normalize from 0:1
-        # 2) Normalize within each sample (more like 0 mean)
-        # 3) Standardize
-
-        # 4) Standardize each sample --> this makes the most sense
+        #  Standardize each sample --> this makes the most sense
         self.training_input, self.testing_input = self.get_standardized_samples()
+        self.training_input_vol, self.testing_input_vol = self.create_vix_input()
 
-        # Get targets
-        # Several options:
-        #   1) Predict change in vol
+        # Get targets to predict change in vol
         self.training_output, self.testing_output = self.target_data_delta()
+        if train:
+            if combined:
+                self.model = self.define_combined_lstm_model()
+                self.train_combined_model()
+            else:
+                self.model = self.define_lstm_model()
+                self.train_model()
+        if combined:
+            self.load_model('combined_lstm_model')
+            self.eval_combined_model()
+        else:
+            self.load_model()
+            self.eval_model()
 
-        #   2) seq2seq (basically a volitility model)
-        # self.training_output, self.testing_output = self.target_data_all()
-        #
-        # self.model = self.define_lstm_model()
-        # self.train_model()
+    def get_vix_params(self):
+        vix_data = self.data.training_vix_data_split
 
-        self.load_model()
-        self.eval_model()
+        vix_var = variance(vix_data)
+        vix_mean = mean(vix_data)
+
+        return vix_var, vix_mean
+
+    def create_vix_input(self):
+        training_input_vol = self._get_all_vol_input(self.data.training_data)
+        testing_input_vol = self._get_all_vol_input(self.data.testing_data)
+        return training_input_vol, testing_input_vol
+
+    def _get_all_vol_input(self, vix_list):
+        input_vol = []
+        for sample in vix_list:
+            initial_vol = sample[1][0]
+            std_sample = self.standardize_vix_sample(initial_vol)
+            input_vol.append(std_sample)
+
+        return input_vol
+
+    def standardize_vix_sample(self, vix_val, epsilon=1e-8):
+        return (vix_val - self.vix_mean) / sqrt(self.vix_var + epsilon)
+
+    def unstandardize_vix_sample(self, prediction, epsilon=1e-8):
+        return prediction * sqrt(self.vix_var + epsilon) + - self.vix_mean
 
     def eval_model(self):
         output_arr = []
@@ -43,40 +66,116 @@ class Model:
 
         vix_actual = self.data.testing_vix_data_split[0:9]
         vix_predicted = self.data.testing_vix_data_split[0:9]
+
         start_ind = 1
         end_ind = 10
         for ind, sample in enumerate(self.testing_input):
-            # print(sample)
             reshaped_sample = sample.reshape(1, 1, 10)
             prediction = self.model.predict(reshaped_sample)[0][0]
-            # print(prediction, self.testing_output[ind])
-            output_arr.append(prediction)
+
+            output_arr.append(prediction[0])
+            # actual_arr.append(actual)
             actual_arr.append(self.testing_output[ind])
 
-            # vix_actual.append(self.data.testing_vix_data_split[end_ind])
+            # vix_actual.append(self.data.testing_vix_data_split[start_ind] + actual)
             vix_actual.append(self.data.testing_vix_data_split[start_ind] + self.testing_output[ind])
             vix_predicted.append(self.data.testing_vix_data_split[start_ind] + prediction[0])
+
             start_ind += 1
             end_ind += 1
+
+        print('DELTA:', np.corrcoef(output_arr, actual_arr))
+        print('TOTAL:', np.corrcoef(vix_predicted, vix_actual))
 
         plt.figure()
         plt.plot(output_arr, label='Output')
         plt.plot(actual_arr, label='Actual')
-        plt.ylabel('Predicted Change in Volitility')
+        plt.ylabel('Predicted Change in Volatility', fontsize=18)
+        plt.title('Predicted vs Actual Change in Volatility', fontsize=18)
         plt.legend()
         plt.show()
-
-        print(vix_actual)
-        print(vix_predicted)
 
         plt.figure()
         plt.plot(vix_predicted, label='Output')
         plt.plot(vix_actual, label='Actual')
-        plt.ylabel('Predicted Value of Volitility')
-        plt.title('Predicted vs Actual Volitility')
+        plt.ylabel('Predicted Value of Volatility', fontsize=18)
+        plt.title('Predicted vs Actual Volatility', fontsize=18)
         plt.legend()
         plt.show()
 
+    def eval_combined_model(self):
+        output_arr = []
+        actual_arr = []
+
+        vix_actual = self.data.testing_vix_data_split[0:9]
+        vix_predicted = self.data.testing_vix_data_split[0:9]
+
+        start_ind = 1
+        end_ind = 10
+        for ind, sample in enumerate(self.testing_input):
+            reshaped_sample = sample.reshape(1, 1, 10)
+
+            vix_input = self.testing_input_vol[ind]
+            std_vix_input = self.standardize_vix_sample(vix_input)
+            reshaped_vix_sample = std_vix_input.reshape(1, 1, 1)
+
+            prediction = self.model.predict({"vol_input": reshaped_vix_sample, "lstm_input":reshaped_sample})[0][0]
+
+            output_arr.append(prediction[0])
+            actual_arr.append(self.testing_output[ind])
+
+            vix_actual.append(self.data.testing_vix_data_split[start_ind] + self.testing_output[ind])
+            vix_predicted.append(self.data.testing_vix_data_split[start_ind] + prediction[0])
+
+            start_ind += 1
+            end_ind += 1
+
+        print('DELTA:', np.corrcoef(output_arr, actual_arr))
+        print('TOTAL:', np.corrcoef(vix_predicted, vix_actual))
+
+        plt.figure()
+        plt.plot(output_arr, label='Output')
+        plt.plot(actual_arr, label='Actual')
+        plt.ylabel('Predicted Change in Volatility', fontsize=18)
+        plt.title('Predicted vs Actual Change in Volatility, Combined', fontsize=18)
+        plt.legend()
+        plt.show()
+
+        plt.figure()
+        plt.plot(vix_predicted, label='Output')
+        plt.plot(vix_actual, label='Actual')
+        plt.ylabel('Predicted Value of Volatility', fontsize=18)
+        plt.title('Predicted vs Actual Volatility, Combined', fontsize=18)
+        plt.legend()
+        plt.show()
+
+    def define_combined_lstm_model(self):
+        '''
+        LSTM with additional non-time dependent input
+        :return:
+        '''
+
+        # Step 1: Define LSTM input
+        lstm_inputs = tf.keras.Input(shape=(None, 10), name="lstm_input")
+        lstm_layer = tf.keras.layers.LSTM(10, return_sequences=True, return_state=True, activation='relu')
+        lstm_outputs, state_h, state_c = lstm_layer(lstm_inputs)
+
+        # Step 2: Define Vol input
+        init_vol_inputs = tf.keras.Input(shape=(None, 1), name="vol_input")
+
+        # Concatenate LSTM output with the initial volitility
+        concat_layer = tf.keras.layers.concatenate([init_vol_inputs, lstm_outputs])
+
+        # Output a single numerical value
+        output_layer = tf.keras.layers.Dense(1)
+        network_output = output_layer(concat_layer)
+        lstm_model = tf.keras.Model(inputs=[init_vol_inputs, lstm_inputs], outputs=network_output)
+
+        # Compile model
+        lstm_model.compile(optimizer="adam", loss="mse", metrics=["accuracy"])
+        print(lstm_model.summary())
+
+        return lstm_model
 
     def define_lstm_model(self):
         '''
@@ -99,16 +198,21 @@ class Model:
         print(lstm_model.summary())
         return lstm_model
 
-    def load_model(self):
-        self.model = tf.keras.models.load_model('lstm_model')
-        print('Loaded model')
+    def load_model(self, name='lstm_model'):
+        self.model = tf.keras.models.load_model(name)
+        print('Loaded model:', name)
 
     def train_model(self):
         reshaped_input = np.asarray(self.training_input).reshape(len(self.training_input), 1, 10)
-        history = self.model.fit(reshaped_input,  np.asarray(self.training_output), epochs=50, validation_split=0.2, batch_size=5)
-
+        self.model.fit(reshaped_input,  np.asarray(self.training_output), epochs=50, validation_split=0.2, batch_size=5)
         self.model.save('lstm_model')
 
+    def train_combined_model(self):
+        reshaped_lstm_input = np.asarray(self.training_input).reshape(len(self.training_input), 1, 10)
+        reshaped_vol_input = np.asarray(self.training_input_vol).reshape(len(self.training_input_vol), 1, 1)
+
+        self.model.fit({"vol_input": reshaped_vol_input, "lstm_input": reshaped_lstm_input}, np.asarray(self.training_output), epochs=50, validation_split=0.2, batch_size=5)
+        self.model.save('combined_lstm_model')
 
     # todo: preprocess this too?
     def target_data_delta(self):
@@ -118,9 +222,6 @@ class Model:
         '''
         training_targets = []
         for sample in self.data.training_data:
-            v_diff = self._vol_diff(sample)
-            # print(v_diff)
-            # print(v_diff + sample[1][0], sample[1][-1])
             training_targets.append(self._vol_diff(sample))
 
         testing_targets = []
